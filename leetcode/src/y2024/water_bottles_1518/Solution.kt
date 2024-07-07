a9cecc8a8030d076eda831eb6fc851240f95c271
nhoxbypass <nhoxbypass@gmail.com>
2024-07-08 01:35:27 +0700

    Leetcode: water_bottles_1518

---
+++ leetcode/src/y2024/water_bottles_1518/Solution.kt
@@ -0,0 +1,28 @@
+package y2024.water_bottles_1518
+
+class Solution {
+    fun numWaterBottles(numBottles: Int, numExchange: Int): Int {
+        // drink initial bottles
+        var drinkCount = numBottles
+        var currEmptyBottles = numBottles
+        while (true) {
+            // exchange new bottles (from empty -> full)
+            val newFullBottles = currEmptyBottles / numExchange
+            if (newFullBottles == 0) {
+                // oops
+                break
+            }
+            // drink new full bottles
+            drinkCount += newFullBottles
+            // calculate empty bottles for new exchange
+            currEmptyBottles = newFullBottles + (currEmptyBottles % numExchange)
+        }
+        return drinkCount
+    }
+}
+
+
+fun main() {
+    val res = Solution().numWaterBottles(5, 6)
+    println(res)
+}
