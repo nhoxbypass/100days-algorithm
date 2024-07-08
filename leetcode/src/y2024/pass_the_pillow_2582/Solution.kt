2d9582b2049635bd6a5b6a36a88fe2a6928d324c
nhoxbypass <nhoxbypass@gmail.com>
2024-07-09 00:45:04 +0700

    Add explains for pass_the_pillow_2582

--- leetcode/src/y2024/pass_the_pillow_2582/Solution.kt
+++ leetcode/src/y2024/pass_the_pillow_2582/Solution.kt
@@ -2,11 +2,19 @@ package y2024.pass_the_pillow_2582
 
 class Solution {
     fun passThePillow(n: Int, time: Int): Int {
-        val stepNeedPerLoop = n-1
+        // calculate steps need for each loop
+        val stepNeedPerLoop = n - 1
+        // check full loop or not
+        // Ex:
+        // - START -> END -> on the way back to START ===> false
+        // - START -> on the way to END ===> true
+        // - START -> END -> START -> on the way to END ===> true
         val fullLoop = (time / stepNeedPerLoop) % 2 == 0
         return if (fullLoop) {
+            // count position from START
             1 + (time % stepNeedPerLoop)
         } else {
+            // count position from END
             n - (time % stepNeedPerLoop)
         }
     }
