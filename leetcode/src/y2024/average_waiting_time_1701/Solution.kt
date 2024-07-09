77fd640b8eda91ba944da495b59db551a84ce8a8
nhoxbypass <nhoxbypass@gmail.com>
2024-07-10 03:36:09 +0700

    Leetcode: average_waiting_time_1701

---
+++ leetcode/src/y2024/average_waiting_time_1701/Solution.kt
@@ -0,0 +1,32 @@
+package y2024.average_waiting_time_1701
+
+
+class Solution {
+    fun averageWaitingTime(customers: Array<IntArray>): Double {
+        if (customers.isEmpty())
+            return 0.0
+
+        var totalWaitTime = 0.0
+        var prevOrderDoneTime = 0
+        val n = customers.size
+        for (i in 0 until n) {
+            // calculate waiting time of curr customer
+            // currOrderDoneTime = max(currArriveTime, prevOrderDoneTime) + currPreparationTime
+            val currOrderDoneTime = (customers[i][0].coerceAtLeast(prevOrderDoneTime)) + customers[i][1]
+            val waitTime = currOrderDoneTime - customers[i][0]
+            // increase total waiting time
+            totalWaitTime += waitTime
+            // mark new done time
+            prevOrderDoneTime = currOrderDoneTime
+        }
+
+        return totalWaitTime / n
+    }
+}
+
+
+fun main() {
+    val customers = arrayOf(intArrayOf(1, 2), intArrayOf(2, 5), intArrayOf(4, 3))
+    val res = Solution().averageWaitingTime(customers)
+    println(res)
+}
