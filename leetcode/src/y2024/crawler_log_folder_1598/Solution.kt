14d672e878f5003a4d4f376254e4b9be09574fbc
nhoxbypass <nhoxbypass@gmail.com>
2024-07-10 23:26:14 +0700

    Leetcode: crawler_log_folder_1598

---
+++ leetcode/src/y2024/crawler_log_folder_1598/Solution.kt
@@ -0,0 +1,36 @@
+package y2024.crawler_log_folder_1598
+
+class Solution {
+    private val BACK = "../"
+    private val STAY = "./"
+
+    fun minOperations(logs: Array<String>): Int {
+        var depth = 0
+        logs.forEach { operation ->
+            when (operation) {
+                BACK -> {
+                    // move back to parent folder
+                    depth--
+                    depth = depth.coerceAtLeast(0)
+                }
+
+                STAY -> {
+                    // stay current folder
+                }
+
+                else -> {
+                    // navigate to folder X
+                    depth++
+                }
+            }
+        }
+        return depth
+    }
+}
+
+
+fun main() {
+    val logs = arrayOf("d1/", "d2/", "../", "d21/", "./")
+    val res = Solution().minOperations(logs)
+    println(res)
+}
