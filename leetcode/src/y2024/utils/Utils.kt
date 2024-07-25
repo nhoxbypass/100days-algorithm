abf59bf0d388e97157b141fad5ba2c84909085f7
nhoxbypass <nhoxbypass@gmail.com>
2024-07-26 00:08:49 +0700

    Leetcode: Add quick sort

--- leetcode/src/y2024/utils/Utils.kt
+++ leetcode/src/y2024/utils/Utils.kt
@@ -37,3 +37,47 @@ fun buildTreeFromBFS(nodes: List<Int?>): TreeNode? {
 
     return root
 }
+
+/**
+ * Quick Sort
+ */
+fun quickSort(nums: IntArray, low: Int, high: Int) {
+    if (low >= high)
+        return
+
+    // partition into 2 part
+    val pivotIdx = partition(nums, low, high)
+
+    // recursive quick sort with lower part
+    quickSort(nums, low, pivotIdx - 1)
+    // recursive quick sort with higher part
+    quickSort(nums, pivotIdx + 1, high)
+}
+
+private fun partition(nums: IntArray, low: Int, high: Int): Int {
+    val pivot = nums[high]
+
+    // do the partition
+    var lowIdx = low
+    for (i in low..high) {
+        if (nums[i] < pivot) {
+            // swap smaller number to the left of pivot
+            swap(nums, lowIdx, i)
+            lowIdx++
+        } else {
+            // do nothing, keep the position of bigger number
+        }
+    }
+
+    // swap pivot to the middle position (which split the array into 2 part)
+    swap(nums, lowIdx, high)
+
+    // return the index of pivot item
+    return lowIdx
+}
+
+private fun swap(nums: IntArray, firstIdx: Int, secondIdx: Int) {
+    if (firstIdx != secondIdx) {
+        nums[firstIdx] = nums[secondIdx].also { nums[secondIdx] = nums[firstIdx] }
+    }
+}
